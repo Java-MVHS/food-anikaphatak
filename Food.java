9b27544877fd51691d6bc7ad6bfa1cefa029c770
anikaphatak <aphatak469@student.fuhsd.org>
2024-10-25 17:07:34 -0700

    Update Food.java

--- Food.java
+++ Food.java
@@ -4,6 +4,12 @@
  * 10/23/2024
  * Food.java
  * 
+ * Purpose: Food.java is the parent class of Pizza.java, the purpose of this program is to print the name of a food
+ * the method used to prepare the food and the food's ingredient 
+ * Working on:
+ * 1. Inheritance and subclasses
+ * 2. Using methods from other classes
+ * 3. Initializing and creating an instance of classes to call methods
  */
  
  public class Food
