704a58aba163a743f4153348c7f9b0c2ef7000bc
anikaphatak <aphatak469@student.fuhsd.org>
2024-10-25 17:20:26 -0700

    Update DeepDishPizza.java

--- DeepDishPizza.java
+++ DeepDishPizza.java
@@ -2,9 +2,9 @@
  * Anika Phatak
  * Period 5
  * 10/23/2024
- * DeepDishPizza3.java
+ * DeepDishPizza.java
  * 
- * DeepDishPizza3 is a subclass of Pizza3, Pizza3 is a subclass of Food, so DeepDishPizza3 orginates from Food
+ * DeepDishPizza is a subclass of Pizza, Pizza is a subclass of Food, so DeepDishPizza orginates from Food
  *
  * The purpose of this program is to print three lines with the type of food
  * cost of food, number of the food item sold and how the food is prepared
@@ -17,7 +17,7 @@
  
  public class DeepDishPizza extends Pizza
  {
-	 public DeepDishPizza()
+	 public DeepDishPizza()		//Create a default constructor with no arguments 
 	 {
 	 }
 	 
